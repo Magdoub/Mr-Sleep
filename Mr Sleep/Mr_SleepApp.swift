eed83c88e6ebb37e567ff437236da4834c5bf353
Mahmoud El Magdoub <m.magdoub@gmail.com>
2025-08-17 19:52:52 +0300

    Initial Commit

---
+++ Mr Sleep/Mr_SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mr_SleepApp.swift
+//  Mr Sleep
+//
+//  Created by Magdoub on 17/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mr_SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
